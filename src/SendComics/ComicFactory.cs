d40b897a282177ddabf87514c8baaaaf43e3621f
Blair Conrad <blair@blairconrad.com>
2024-02-11 13:47:24 -0500

    Use switch expression in ComicFactory

--- src/SendComics/ComicFactory.cs
+++ src/SendComics/ComicFactory.cs
@@ -1,24 +1,16 @@
 namespace SendComics
 {
-    using System;
     using Comics;
     using Services;
 
     internal static class ComicFactory
     {
-        public static Comic GetComic(string name, IComicFetcher comicFetcher)
-        {
-            if (name == "blondie" || name == "rhymes-with-orange" || name == "bizarro")
+        public static Comic GetComic(string name, IComicFetcher comicFetcher) =>
+            name switch
             {
-                return new ComicsKingdomComic(name, comicFetcher);
-            }
-
-            if (name == "schlockmercenary")
-            {
-                return new SchlockMercenaryComic(comicFetcher);
-            }
-
-            return new GoComic(name, comicFetcher);
-        }
+                "blondie" or "rhymes-with-orange" or "bizarro" => new ComicsKingdomComic(name, comicFetcher),
+                "schlockmercenary" => new SchlockMercenaryComic(comicFetcher),
+                _ => new GoComic(name, comicFetcher),
+            };
     }
 }
