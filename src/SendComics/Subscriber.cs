b4aad64e167d4b669639cb32852faabed2f0d10c
Blair Conrad <blair@blairconrad.com>
2024-11-22 07:01:03 -0500

    Make Subscriber.subscriptions an IEnumerable
    This caused it to fit better with the primary constructor somehow!

--- src/SendComics/Subscriber.cs
+++ src/SendComics/Subscriber.cs
@@ -4,12 +4,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public class Subscriber(string email, Subscription[] subscriptions)
+public class Subscriber(string email, IEnumerable<Subscription> subscriptions)
 {
-    private IEnumerable<Subscription> subscriptions = subscriptions;
-
     public string Email { get; } = email;
 
     internal IEnumerable<Episode> GetEpisodesFor(DateTime today) =>
-        this.subscriptions.SelectMany(s => s.GetEpisodesFor(today));
+        subscriptions.SelectMany(s => s.GetEpisodesFor(today));
 }
